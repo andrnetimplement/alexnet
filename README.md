4b35939f3900fb6f2ebea8a5055d98e729f7a3b6
Andrey Nemerov <andrey.nemerov@rt.ru>
2024-06-20 10:47:42 +0300

    updt

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # alexnet
+
+Implementation of the neural network described here: https://proceedings.neurips.cc/paper_files/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-import time
 from model.alexnet import AlexNet
 from utils.trainer import train_model, evaluate_model
 
